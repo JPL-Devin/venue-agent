00387fc1ffa15649f82c9742f877fb10a5ea6526
swanchr <christopher.a.swan@jpl.nasa.gov>
2025-09-17 17:08:24 +0000

    removed ampcs tests

--- tests/venue_client.py
+++ tests/venue_client.py
@@ -1,17 +1,13 @@
 import requests
-import sys
 import os
 import time
 import jwt
-import socket
+
 
 PRIVATE_PEM_FILE = 'exec_venue_private_pem.pem'
 
 server_1 = 'http://localhost:19443/api/v3'
 server_2 = 'http://localhost:19444/api/v3'
-
-session_id_a = int(os.environ.get('TEST_AMPCS_SESSION_ID_A', 0))
-session_id_b = int(os.environ.get('TEST_AMPCS_SESSION_ID_B', 0))
 
 private_pem_path = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 
                                PRIVATE_PEM_FILE)
@@ -42,173 +38,6 @@ def generate_exec_token():
 generate_exec_token()
 
 ###
-hostname = socket.gethostname()
-if hostname.startswith('eurc'):
-    CMD_NO_OP = 'CMD_NO_OP'
-    CMD_COMP_EVR = 'CMD_SVC_EVR_CMD_COMPLETED_SUCCESS'
-    CMD_COUNTER_EHA = 'CMD-0027'
-    CMD_DOWN_DP = 'DDM_MANAGE_DWN_PB,DATA_PRODUCTS,ENABLE'
-    DP_AP_ID = 37 # DP_EVR_REC_ACT_LOW
-
-elif hostname.startswith('psyche'):
-    CMD_NO_OP = 'CMD_NO_OP'
-    CMD_COMP_EVR = 'CMD_SVC_EVR_CMD_COMPLETED_SUCCESS'
-    CMD_COUNTER_EHA = 'CMD-0002'
-    CMD_DOWN_DP = 'DWN_MANAGE_PB,DATA_PRODUCTS,ENABLE'
-    DP_AP_ID = 107 # DP_EHA_RECORDED
-else:
-    print(f'Error: Unrecognized hostname: {hostname}')
-    sys.exit(1)
-
-
-def start_mtak(server, sessionIds, timeout=60, defaultCmdString='AB'):
-    url = f'{server}/mtak/start'
-    res = requests.post(url, 
-	    json={'sessionIds': sessionIds, 'timeout': timeout, 'defaultCmdString': defaultCmdString},
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def shutdown_mtak(server):
-    url = f'{server}/mtak/shutdown'
-    res = requests.post(url, headers=exec_shared_dict['headers'])
-    return res
-
-def send_fsw_cmd(server, sessionId, commandString, validate=False, stringSelection='DEFAULT', timeout=10):
-    print(f'commandString: {commandString}')
-    url = f'{server}/cmd/fsw_cmd'
-    res = requests.post(url, json={
-            'sessionId': sessionId, 
-            'commandString': commandString, 
-            'validate': validate,
-            'stringSelection': stringSelection,
-            'timeout': timeout
-    	},
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def send_sse_cmd(server, sessionId, commandString, timeout=10):
-    url = f'{server}/cmd/sse'
-    res = requests.post(url, json={
-            'sessionId': sessionId,
-            'commandString': commandString,
-            'timeout': timeout
-        },
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def query_rt_evr(server, sessionId, evrName, startTime=None, endTime=None, timeout=240):
-    url = f'{server}/evr/realtime'
-    payload = {
-        'sessionId': sessionId,
-        'evrName': evrName,
-        'startTime': startTime,
-        'endTime': endTime,
-        'timeout': timeout
-    }
-
-    res = requests.get(url, 
-        json=payload,
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def query_chill_evr(server, sessionId, evrName, startTime=None, endTime=None, timeout=240):
-    url = f'{server}/evr/chill'
-    payload = {
-        'sessionId': sessionId,
-        'evrName': evrName,
-        'startTime': startTime,
-        'endTime': endTime,
-        'timeout': timeout
-    }
-
-    res = requests.get(url, 
-        json=payload,
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def query_rt_eha(server, sessionId, channelId, startTime=None, endTime=None, timeout=240):
-    url = f'{server}/eha/realtime'
-    payload = {
-        'sessionId': sessionId,
-        'channelId': channelId,
-        'startTime': startTime,
-        'endTime': endTime,
-        'timeout': timeout
-    }
-
-    res = requests.get(url, 
-        json=payload,
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def query_chill_eha(server, sessionId, channelId, timeType='ERT', startTime=None, endTime=None, timeout=240):
-    url = f'{server}/eha/chill'
-    payload = {
-        'sessionId': sessionId,
-        'channelIds': [channelId],
-        'timeType': timeType,
-        'startTime': startTime,
-        'endTime': endTime,
-        'timeout': timeout
-    }
-
-    res = requests.get(url, 
-        json=payload,
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def query_dp(server, sessionId, dpStatus='ALL', apIds=[], timeType='ERT', startTime=None, endTime=None, timeout=240):
-    url = f'{server}/dp'
-    payload = {
-        'sessionId': sessionId,
-        'dpStatus': dpStatus,
-        'apIds': apIds,
-        'timeType': timeType,
-        'startTime': startTime,
-        'endTime': endTime,
-        'timeout': timeout
-    }
-
-    res = requests.get(url, 
-        json=payload,
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-def send_file(server, sessionId, sourceFilePath, targetFilePath, overwrite=True, fileType=0, stringSelection='DEFAULT', timeout=10):
-    url = f'{server}/cmd/binary_file'
-    res = requests.post(url, json={
-            'sessionId': sessionId,
-            'sourceFilePath': sourceFilePath,
-            'targetFilePath': targetFilePath,
-            'overwrite': True,
-            'fileType': 0,
-            'stringSelection': 'DEFAULT',
-            'timeout': 60
-        },
-        headers=exec_shared_dict['headers']
-    )
-    return res
-
-
-def send_scmf_file(server, sessionId, filePath, disableChecks=False, timeout=10):
-    url = f'{server}/cmd/scmf'
-    res = requests.post(url, json={
-            'sessionId': sessionId,
-            'filePath': filePath,
-            'disableChecks': disableChecks,
-            'timeout': 60
-        },
-        headers=exec_shared_dict['headers']
-    )
-    return res
 
 def run_custom_script(server, startInput):
     url = f'{server}/custom_script/start'
