54613f9dd9f59639643e52b8484a346b4d883640
swanchr <christopher.a.swan@jpl.nasa.gov>
2025-09-17 17:13:22 +0000

    update env, remove psyche/europa

---
+++ config/project_dev_envs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# VENUE SERVER
+export ING_VENUE_DIR=<local venue directory>
+export ING_LOG_DIR=<local log directory>
+
+# Custom Script
+export CUSTOM_SCRIPT_BASE_DIR=<custom script director>
