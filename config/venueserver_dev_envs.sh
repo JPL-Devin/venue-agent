3d8bb3a1d61ca0c3dc9756effa5d194c6f154f9a
swanchr <christopher.a.swan@jpl.nasa.gov>
2025-10-21 15:36:58 +0000

    switched to generic paths

--- config/venueserver_dev_envs.sh
+++ config/venueserver_dev_envs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # VENUE SERVER
-export ING_VENUE_DIR=/home/hongmank/github/venue-agent
-export ING_LOG_DIR=/home/hongmank/github/venue-agent
+export ING_VENUE_DIR=/home/ing-user/github/venue-agent
+export ING_LOG_DIR=/home/ing-user/github/venue-agent
 
 # Custom Script
-export CUSTOM_SCRIPT_BASE_DIR=/proj/europa/sit
+export CUSTOM_SCRIPT_BASE_DIR=/home/ing-user/custom_scripts
